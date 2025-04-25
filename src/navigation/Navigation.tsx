d268ffa0b5c99f8897b7b82c56837916000a2df7
Abhik Baidya <abhikbaidya7@gmail.com>
2025-04-25 12:15:19 +0530

    Navigation added

---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,27 @@
+import React, {FC} from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {navigationRef} from '../utils/NavigationUtils';
+import SplashScreen from '../features/auth/SplashScreen';
+import LoginScreen from '../features/auth/LoginScreen';
+import RegisterScreen from '../features/auth/RegisterScreen';
+
+const Stack = createNativeStackNavigator();
+const Navigation: FC = () => {
+  return (
+    <NavigationContainer ref={navigationRef}>
+      <Stack.Navigator
+        initialRouteName="SplashScreen"
+        screenOptions={{
+          headerShown: false,
+          animation: 'fade',
+        }}>
+        <Stack.Screen name="SplashScreen" component={SplashScreen} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
