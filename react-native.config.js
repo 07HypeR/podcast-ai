0be993a53eeac86be9332317973f3e80bc168556
Abhik Baidya <abhikbaidya7@gmail.com>
2025-04-25 10:32:31 +0530

    Config file changed

--- react-native.config.js
+++ react-native.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-  project: {
-    ios: {},
-    android: {},
-  },
-  'react-native-vector-icons': {
-    platforms: {
-      ios: null,
-    },
-  },
   assets: ['./src/assets/fonts'],
   getTransformModulePath() {
     return require.resolve('react-native-typescript-transformer');
