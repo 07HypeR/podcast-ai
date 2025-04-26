58a85f8735d78f39bdf38ca8fe2c99bffeda342c
Abhik Baidya <abhikbaidya7@gmail.com>
2025-04-26 12:27:57 +0530

    Login & Register screen added

---
+++ src/features/auth/LoginScreen.tsx
@@ -0,0 +1,100 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
+import {Colors} from '../../utils/Constants';
+import {screenHeight, screenWidth} from '../../utils/Scaling';
+import CustomText from '../../components/ui/CustomText';
+import {navigate} from '../../utils/NavigationUtils';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../../assets/icons/logo.png')}
+        style={styles.logoImage}
+      />
+      <CustomText variant="h3" style={styles.header}>
+        Login
+      </CustomText>
+      <TextInput
+        value={email}
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor={Colors.inactive}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        placeholderTextColor={Colors.inactive}
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <CustomText variant="h5" style={styles.buttonText}>
+          {false ? 'Logging in...' : 'Login'}
+        </CustomText>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigate('RegisterScreen')}>
+        <CustomText variant="h6" style={styles.signUpText}>
+          Don't have an account? Sign Up
+        </CustomText>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.backgroundDark,
+    alignItems: 'center',
+    padding: 20,
+  },
+  header: {
+    marginBottom: 20,
+    color: Colors.text,
+  },
+  logoImage: {
+    height: screenHeight * 0.15,
+    marginTop: 50,
+    width: screenWidth * 0.6,
+    resizeMode: 'contain',
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    backgroundColor: Colors.backgroundLight,
+    borderRadius: 8,
+    paddingHorizontal: 15,
+    color: Colors.text,
+    marginBottom: 15,
+  },
+  button: {
+    width: '100%',
+    height: 50,
+    backgroundColor: Colors.primary,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  buttonText: {
+    color: Colors.background,
+  },
+  signUpText: {
+    marginTop: 15,
+    color: Colors.primary,
+  },
+});
+export default LoginScreen;
